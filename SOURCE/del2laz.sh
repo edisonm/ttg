bcfd088ef963ca0788a122e56a5650fc3845a088
edison <edison@68707deb-da3f-46f8-829b-9c434defd12b>
2010-03-19 23:44:32 +0000

    Minor change.
    git-svn-id: svn+ssh://mntedison/home/edison/LocalReps/ttg/trunk@50 68707deb-da3f-46f8-829b-9c434defd12b

--- SOURCE/del2laz.sh
+++ SOURCE/del2laz.sh
@@ -6,11 +6,16 @@
 ## Login   <edison@vaio2edison>
 ## 
 ## Started on  Sun Mar  7 01:21:08 2010 Edison Mera
-## Last update Thu Mar 18 00:06:59 2010 Edison Mera
+## Last update Sat Mar 20 00:44:07 2010 Edison Mera
 ##
 
-find . -name "*.pas" -exec str_replace "TkbmMemTable" "TMemDataset" {} \;
-find . -name "*.dfm" -exec str_replace "TkbmMemTable" "TMemDataset" {} \;
-find . -name "*.pas" -exec str_replace "TRxDrawGrid" "TDrawGrid" {} \;
-find . -name "*.dfm" -exec str_replace "TRxDrawGrid" "TDrawGrid" {} \;
-find . -name "*.pas" -exec str_replace " kbmMemTable," "" {} \;
+find . -name "*.pas" -exec str_replace "TkbmMemTable"    "TMemDataset" {} \;
+find . -name "*.dfm" -exec str_replace "TkbmMemTable"    "TMemDataset" {} \;
+find . -name "*.pas" -exec str_replace "TRxDrawGrid"       "TDrawGrid" {} \;
+find . -name "*.dfm" -exec str_replace "TRxDrawGrid"       "TDrawGrid" {} \;
+find . -name "*.pas" -exec str_replace "kbmMemTable"           "memds" {} \;
+find . -name "*.pas" -exec str_replace "AttachedAutoRefresh = True" "" {} \;
+find . -name "*.pas" -exec str_replace "AutoIncMinValue = -1"       "" {} \;
+find . -name "*.pas" -exec str_replace "EnableIndexes = True"       "" {} \;
+find . -name "*.pas" -exec str_replace "AutoReposition = True"      "" {} \;
+
