98d253f32c07494d9f9b19c5138c3d2042149004
edison <edison@68707deb-da3f-46f8-829b-9c434defd12b>
2010-04-24 14:00:45 +0000

    Minor improvement.
    git-svn-id: svn+ssh://mntedison/home/edison/LocalReps/ttg/trunk@138 68707deb-da3f-46f8-829b-9c434defd12b

--- SOURCE/del2laz.sh
+++ SOURCE/del2laz.sh
@@ -6,12 +6,10 @@
 ## Login   <edison@vaio2edison>
 ## 
 ## Started on  Sun Mar  7 01:21:08 2010 Edison Mera
-## Last update Sat Apr 24 15:38:58 2010 Edison Mera
+## Last update Sat Apr 24 16:00:34 2010 Edison Mera
 ##
 
 find . -name "*.pas" -exec str_replace "TkbmMemTable"    "TDbf" {} \;
 find . -name "*.dfm" -exec str_replace "TkbmMemTable"    "TDbf" {} \;
-find . -name "*.pas" -exec str_replace "kbmMemTable"           "dbf" {} \;
-
-
-
+find . -name "*.pas" -exec str_replace "kbmMemTable"     "dbf" {} \;
+find . -name "*.pas" -exec str_replace "TIntegerField"   "TLongIntField" {} \;
