63f5fcecb4f66982c445d20480af326e6d32a61a
edison <edison@68707deb-da3f-46f8-829b-9c434defd12b>
2010-04-24 14:23:40 +0000

    Minor changes.
    git-svn-id: svn+ssh://mntedison/home/edison/LocalReps/ttg/trunk@142 68707deb-da3f-46f8-829b-9c434defd12b

--- SOURCE/del2laz.sh
+++ SOURCE/del2laz.sh
@@ -6,12 +6,15 @@
 ## Login   <edison@vaio2edison>
 ## 
 ## Started on  Sun Mar  7 01:21:08 2010 Edison Mera
-## Last update Sat Apr 24 16:15:29 2010 Edison Mera
+## Last update Sat Apr 24 16:22:55 2010 Edison Mera
 ##
 
 find . -name "*.pas" -exec str_replace "TkbmMemTable"    "TDbf" {} \;
 find . -name "*.dfm" -exec str_replace "TkbmMemTable"    "TDbf" {} \;
 find . -name "*.pas" -exec str_replace "kbmMemTable"     "dbf" {} \;
 find . -name "*.pas" -exec str_replace "TIntegerField"   "TLongIntField" {} \;
+find . -name "*.dfm" -exec str_replace "TIntegerField"   "TLongIntField" {} \;
 find . -name "*.pas" -exec str_replace "Windows,"        "{Windows,}" {} \;
-find . -name "*.pas" -exec str_replace "Mask,"        "{Mask,}" {} \;
+find . -name "*.pas" -exec str_replace "Consts,"         "{Consts,}" {} \;
+find . -name "*.pas" -exec str_replace "Mask,"           "{Mask,}" {} \;
+find . -name "*.pas" -exec str_replace "BDE,"            "{BDE,}" {} \;
