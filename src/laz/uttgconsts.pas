1b241db87e3229cb2e239232868433b7e9063826
edison <edison@68707deb-da3f-46f8-829b-9c434defd12b>
2012-02-01 00:17:17 +0000

    Added constants
    git-svn-id: svn+ssh://mntedison/home/edison/LocalReps/ttg/trunk@548 68707deb-da3f-46f8-829b-9c434defd12b

---
+++ src/laz/uttgconsts.pas
@@ -0,0 +1,28 @@
+unit UTTGConsts;
+
+{$mode objfpc}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+resourcestring
+  SNumSoftTeacherRestrictions = 'Numero de prohibiciones de profesores sin problemas...';
+  SNumHardTeacherRestrictions = 'Numero de prohibiciones de profesores con problemas...';
+  STeacherRestrictionsHead = 'Teacher; Restrictions';
+  STeachersWorkLoadWithoutProblems = 'Carga horaria de los profesores sin problemas...';
+  STeachersWorkLoadWithProblems = 'Carga horaria de los profesores con problemas...';
+  STeacherWorkLoadHead = 'Teacher; Work Load';
+  SRoomTypesWithoutProblems = 'Tipos de aulas sin problemas...';
+  SRoomTypesWithProblems = 'Tipos de aulas con problemas...';
+  SRoomTypesLoadHead = 'RoomType; Available Hours; Load';
+  SClassWorkLoadWithoutProblems = 'Carga horaria de paralelos sin problemas...';
+  SClassWorkLoadWithProblems = 'Carga Hourria de paralelos con problemas...';
+  SClassWorkLoadHead = 'Class; Work Load';
+  SProblems = 'Problemas';
+  SSubject = 'Materia';
+implementation
+
+end.
+
