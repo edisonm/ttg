5950fbf201b379eb07ab3d7fb65e17135e4d14fd
edison <edison@68707deb-da3f-46f8-829b-9c434defd12b>
2012-02-06 15:29:04 +0000

    More translations
    git-svn-id: svn+ssh://mntedison/home/edison/LocalReps/ttg/trunk@594 68707deb-da3f-46f8-829b-9c434defd12b

---
+++ utils/translate_lcl.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+find . -name "*.po"  -exec str_replace "msgid \"$1\"" "msgid \"$2\"" {} \;
+find . -name "*.lrt" -exec str_replace "=$1^" "=$2^" {} \;
+find . -name "*.lfm" -exec str_replace "= '$1'" "= '$2'" {} \;
+
