d307cf9e3ad6339adb307fe56407545e575c8fd9
edison <edison@68707deb-da3f-46f8-829b-9c434defd12b>
2012-02-17 00:56:57 +0000

    Minor
    git-svn-id: svn+ssh://mntedison/home/edison/LocalReps/ttg/trunk@722 68707deb-da3f-46f8-829b-9c434defd12b

--- dat/ttg.sql
+++ dat/ttg.sql
@@ -11,31 +11,31 @@
 */
 CREATE TABLE IF NOT EXISTS `Parallel`(
     `IdParallel` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT /* Parallel Id */,
-    `NaParallel` varchar(5) NOT NULL UNIQUE /* Parallel Name */
+    `NaParallel` VARCHAR(5) NOT NULL UNIQUE /* Parallel Name */
 ); /* Parallels */
 CREATE TABLE IF NOT EXISTS `Day`(
     `IdDay` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT /* Day Id */,
-    `NaDay` varchar(10) NOT NULL UNIQUE /* Name of Day */
+    `NaDay` VARCHAR(10) NOT NULL UNIQUE /* Name of Day */
 ); /* Working Days */
 CREATE TABLE IF NOT EXISTS `Category`(
     `IdCategory` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT /* Category Id */,
-    `NaCategory` varchar(25) NOT NULL UNIQUE /* Category Name */,
-    `AbCategory` varchar(5) NOT NULL UNIQUE /* Category Abbreviation */
+    `NaCategory` VARCHAR(25) NOT NULL UNIQUE /* Category Name */,
+    `AbCategory` VARCHAR(5) NOT NULL UNIQUE /* Category Abbreviation */
 ); /* Categories */
 CREATE TABLE IF NOT EXISTS `Hour`(
     `IdHour` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT /* Hour Id */,
-    `NaHour` varchar(10) NOT NULL UNIQUE /* Hour Name */,
-    `Interval` varchar(21) NOT NULL UNIQUE /* Hour Interval */
+    `NaHour` VARCHAR(10) NOT NULL UNIQUE /* Hour Name */,
+    `Interval` VARCHAR(21) NOT NULL UNIQUE /* Hour Interval */
 ); /* Academic Hours */
 CREATE TABLE IF NOT EXISTS `RoomType`(
     `IdRoomType` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT /* Room Type Id */,
-    `NaRoomType` varchar(25) NOT NULL UNIQUE /* Room Type Name */,
-    `AbRoomType` varchar(10) NOT NULL UNIQUE /* Room Type Abbreviation */,
-    `Number` integer NOT NULL /* Number of Rooms */
+    `NaRoomType` VARCHAR(25) NOT NULL UNIQUE /* Room Type Name */,
+    `AbRoomType` VARCHAR(10) NOT NULL UNIQUE /* Room Type Abbreviation */,
+    `Number` INTEGER NOT NULL /* Number of Rooms */
 ); /* Types of Room */
 CREATE TABLE IF NOT EXISTS `Cluster`(
     `IdCategory` INTEGER NOT NULL /* Category */,
-    `IdParallel` integer NOT NULL /* Parallel */,
+    `IdParallel` INTEGER NOT NULL /* Parallel */,
   CONSTRAINT `PrimaryKey` PRIMARY KEY(`IdCategory`,`IdParallel`),
   CONSTRAINT `CategoryCluster` FOREIGN KEY (`IdCategory`)
     REFERENCES `Category`(`IdCategory`) ON UPDATE RESTRICT ON DELETE RESTRICT,
@@ -44,24 +44,24 @@ CREATE TABLE IF NOT EXISTS `Cluster`(
 ); /* Clusters */
 CREATE TABLE IF NOT EXISTS `Theme`(
     `IdTheme` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT /* Theme Id */,
-    `NaTheme` varchar(20) NOT NULL UNIQUE /* Theme Name */
+    `NaTheme` VARCHAR(20) NOT NULL UNIQUE /* Theme Name */
 ); /* Themes */
 CREATE TABLE IF NOT EXISTS `Teacher`(
     `IdTeacher` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT /* Teacher Id */,
-    `TeacherNationalId` varchar(11) NOT NULL /* Teacher National Id */,
-    `LnTeacher` varchar(15) NOT NULL /* Teacher Last Name */,
-    `NaTeacher` varchar(15) NOT NULL /* Teacher Name */,
+    `TeacherNationalId` VARCHAR(11) NOT NULL /* Teacher National Id */,
+    `LnTeacher` VARCHAR(15) NOT NULL /* Teacher Last Name */,
+    `NaTeacher` VARCHAR(15) NOT NULL /* Teacher Name */,
   CONSTRAINT `ixLnNaTeacher` UNIQUE(`LnTeacher`,`NaTeacher`),
   CONSTRAINT `ixNaLnTeacher` UNIQUE(`NaTeacher`,`LnTeacher`)
 ); /* Teachers */
 CREATE TABLE IF NOT EXISTS `Distribution`(
-    `IdTheme` integer NOT NULL /* Theme Id */,
+    `IdTheme` INTEGER NOT NULL /* Theme Id */,
     `IdCategory` INTEGER NOT NULL /* Category Id */,
-    `IdParallel` integer NOT NULL /* Parallel Id */,
-    `IdTeacher` integer NOT NULL /* Teacher Id */,
-    `IdRoomType` integer NOT NULL /* Room Type Id */,
-    `RoomCount` integer /* Number of classrooms needed */,
-    `Composition` varchar(40) NOT NULL /* Composition of the Slots for the Theme */,
+    `IdParallel` INTEGER NOT NULL /* Parallel Id */,
+    `IdTeacher` INTEGER NOT NULL /* Teacher Id */,
+    `IdRoomType` INTEGER NOT NULL /* Room Type Id */,
+    `RoomCount` INTEGER /* Number of classrooms needed */,
+    `Composition` VARCHAR(40) NOT NULL /* Composition of the Slots for the Theme */,
   CONSTRAINT `PrimaryKey` PRIMARY KEY(`IdTheme`,`IdCategory`,`IdParallel`),
   CONSTRAINT `ClusterDistribution` FOREIGN KEY (`IdCategory`,`IdParallel`)
     REFERENCES `Cluster`(`IdCategory`,`IdParallel`) ON UPDATE RESTRICT ON DELETE RESTRICT,
@@ -73,11 +73,11 @@ CREATE TABLE IF NOT EXISTS `Distribution`(
     REFERENCES `Teacher`(`IdTeacher`) ON UPDATE RESTRICT ON DELETE RESTRICT
 ); /* Distribution of Workload */
 CREATE TABLE IF NOT EXISTS `JoinedCluster`(
-    `IdTheme` integer NOT NULL /* Theme Id */,
+    `IdTheme` INTEGER NOT NULL /* Theme Id */,
     `IdCategory` INTEGER NOT NULL /* Category Id */,
-    `IdParallel` integer NOT NULL /* Parallel Id */,
+    `IdParallel` INTEGER NOT NULL /* Parallel Id */,
     `IdCategory1` INTEGER NOT NULL /* Category Id of Joined Cluster */,
-    `IdParallel1` integer NOT NULL /* Parallel Id of Joined Cluster */,
+    `IdParallel1` INTEGER NOT NULL /* Parallel Id of Joined Cluster */,
   CONSTRAINT `PrimaryKey` PRIMARY KEY(`IdTheme`,`IdCategory`,`IdParallel`,`IdCategory1`,`IdParallel1`),
   CONSTRAINT `ClusterJoinedCluster` FOREIGN KEY (`IdCategory1`,`IdParallel1`)
     REFERENCES `Cluster`(`IdCategory`,`IdParallel`) ON UPDATE RESTRICT ON DELETE RESTRICT,
@@ -85,14 +85,14 @@ CREATE TABLE IF NOT EXISTS `JoinedCluster`(
     REFERENCES `Distribution`(`IdTheme`,`IdCategory`,`IdParallel`) ON UPDATE CASCADE ON DELETE CASCADE
 ); /* Joined Clusters */
 CREATE TABLE IF NOT EXISTS `ThemeRestrictionType`(
-    `IdThemeRestrictionType` integer NOT NULL PRIMARY KEY /* Theme Restriction Type Id */,
-    `NaThemeRestrictionType` varchar(10) NOT NULL UNIQUE /* Restriction Type Name */,
-    `ColThemeRestrictionType` integer NOT NULL /* Restriction Type Color */,
-    `ValThemeRestrictionType` integer NOT NULL /* Restriction Type Value */
+    `IdThemeRestrictionType` INTEGER NOT NULL PRIMARY KEY /* Theme Restriction Type Id */,
+    `NaThemeRestrictionType` VARCHAR(10) NOT NULL UNIQUE /* Restriction Type Name */,
+    `ColThemeRestrictionType` INTEGER NOT NULL /* Restriction Type Color */,
+    `ValThemeRestrictionType` INTEGER NOT NULL /* Restriction Type Value */
 ); /* Types of Theme Restrictions */
 CREATE TABLE IF NOT EXISTS `TimeSlot`(
-    `IdDay` integer NOT NULL /* Day Id */,
-    `IdHour` integer NOT NULL /* Hour Id */,
+    `IdDay` INTEGER NOT NULL /* Day Id */,
+    `IdHour` INTEGER NOT NULL /* Hour Id */,
   CONSTRAINT `PrimaryKey` PRIMARY KEY(`IdDay`,`IdHour`),
   CONSTRAINT `DayTimeSlot` FOREIGN KEY (`IdDay`)
     REFERENCES `Day`(`IdDay`) ON UPDATE RESTRICT ON DELETE RESTRICT,
@@ -100,10 +100,10 @@ CREATE TABLE IF NOT EXISTS `TimeSlot`(
     REFERENCES `Hour`(`IdHour`) ON UPDATE RESTRICT ON DELETE RESTRICT
 ); /* Time Slots */
 CREATE TABLE IF NOT EXISTS `Assistance`(
-    `IdTheme` integer NOT NULL /* Theme Id */,
+    `IdTheme` INTEGER NOT NULL /* Theme Id */,
     `IdCategory` INTEGER NOT NULL /* Category Id */,
-    `IdParallel` integer NOT NULL /* Parallel Id */,
-    `IdTeacher` integer NOT NULL /* Teacher Id */,
+    `IdParallel` INTEGER NOT NULL /* Parallel Id */,
+    `IdTeacher` INTEGER NOT NULL /* Teacher Id */,
   CONSTRAINT `PrimaryKey` PRIMARY KEY(`IdTheme`,`IdCategory`,`IdParallel`,`IdTeacher`),
   CONSTRAINT `DistributionAssistance` FOREIGN KEY (`IdTheme`,`IdCategory`,`IdParallel`)
     REFERENCES `Distribution`(`IdTheme`,`IdCategory`,`IdParallel`) ON UPDATE CASCADE ON DELETE CASCADE,
@@ -112,15 +112,15 @@ CREATE TABLE IF NOT EXISTS `Assistance`(
 ); /* Assistances */
 CREATE TABLE IF NOT EXISTS `TeacherRestrictionType`(
     `IdTeacherRestrictionType` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT /* Teacher Restriction Type Id */,
-    `NaTeacherRestrictionType` varchar(10) NOT NULL UNIQUE /* Restriction Type Name */,
-    `ColTeacherRestrictionType` integer NOT NULL /* Restriction Type Color */,
-    `ValTeacherRestrictionType` integer NOT NULL /* Restriction Type Value */
+    `NaTeacherRestrictionType` VARCHAR(10) NOT NULL UNIQUE /* Restriction Type Name */,
+    `ColTeacherRestrictionType` INTEGER NOT NULL /* Restriction Type Color */,
+    `ValTeacherRestrictionType` INTEGER NOT NULL /* Restriction Type Value */
 ); /* Types of Teacher Restrictions */
 CREATE TABLE IF NOT EXISTS `TeacherRestriction`(
-    `IdTeacher` integer NOT NULL /* Teacher Id */,
-    `IdDay` integer NOT NULL /* Day Id */,
-    `IdHour` integer NOT NULL /* Hour Id */,
-    `IdTeacherRestrictionType` integer NOT NULL /* Teacher Restriction Type Id */,
+    `IdTeacher` INTEGER NOT NULL /* Teacher Id */,
+    `IdDay` INTEGER NOT NULL /* Day Id */,
+    `IdHour` INTEGER NOT NULL /* Hour Id */,
+    `IdTeacherRestrictionType` INTEGER NOT NULL /* Teacher Restriction Type Id */,
   CONSTRAINT `PrimaryKey` PRIMARY KEY(`IdTeacher`,`IdDay`,`IdHour`),
   CONSTRAINT `TeacherRestrictionTypeTeacherRestriction` FOREIGN KEY (`IdTeacherRestrictionType`)
     REFERENCES `TeacherRestrictionType`(`IdTeacherRestrictionType`) ON UPDATE RESTRICT ON DELETE RESTRICT,
@@ -130,10 +130,10 @@ CREATE TABLE IF NOT EXISTS `TeacherRestriction`(
     REFERENCES `TimeSlot`(`IdDay`,`IdHour`) ON UPDATE RESTRICT ON DELETE RESTRICT
 ); /* Teacher Restrictions */
 CREATE TABLE IF NOT EXISTS `ThemeRestriction`(
-    `IdTheme` integer NOT NULL /* Theme Id */,
-    `IdDay` integer NOT NULL /* Day Id */,
-    `IdHour` integer NOT NULL /* Hour Id */,
-    `IdThemeRestrictionType` integer NOT NULL /* Theme Restriction Type Id */,
+    `IdTheme` INTEGER NOT NULL /* Theme Id */,
+    `IdDay` INTEGER NOT NULL /* Day Id */,
+    `IdHour` INTEGER NOT NULL /* Hour Id */,
+    `IdThemeRestrictionType` INTEGER NOT NULL /* Theme Restriction Type Id */,
   CONSTRAINT `PrimaryKey` PRIMARY KEY(`IdTheme`,`IdDay`,`IdHour`),
   CONSTRAINT `ThemeRestrictionTypeThemeRestriction` FOREIGN KEY (`IdThemeRestrictionType`)
     REFERENCES `ThemeRestrictionType`(`IdThemeRestrictionType`) ON UPDATE RESTRICT ON DELETE RESTRICT,
@@ -144,18 +144,18 @@ CREATE TABLE IF NOT EXISTS `ThemeRestriction`(
 ); /* Theme Restrictions */
 CREATE TABLE IF NOT EXISTS `Timetable`(
     `IdTimetable` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT /* Timetable Id */,
-    `TimeIni` datetime NOT NULL /* Initial Time */,
-    `TimeEnd` datetime NOT NULL /* End Time */,
+    `TimeIni` DATETIME NOT NULL /* Initial Time */,
+    `TimeEnd` DATETIME NOT NULL /* End Time */,
     `Summary` text /* Summary of Timetable Generation */
 ); /* Timetables */
 CREATE TABLE IF NOT EXISTS `TimetableDetail`(
-    `IdTimetable` integer NOT NULL /* Timetable Id */,
-    `IdTheme` integer NOT NULL /* Theme Id */,
+    `IdTimetable` INTEGER NOT NULL /* Timetable Id */,
+    `IdTheme` INTEGER NOT NULL /* Theme Id */,
     `IdCategory` INTEGER NOT NULL /* Category Id */,
-    `IdParallel` integer NOT NULL /* Parallel Id */,
-    `IdDay` integer NOT NULL /* Day Id */,
-    `IdHour` integer NOT NULL /* Hour Id */,
-    `Session` integer NOT NULL /* Internal Number */,
+    `IdParallel` INTEGER NOT NULL /* Parallel Id */,
+    `IdDay` INTEGER NOT NULL /* Day Id */,
+    `IdHour` INTEGER NOT NULL /* Hour Id */,
+    `Session` INTEGER NOT NULL /* Internal Number */,
   CONSTRAINT `ixRestrictionTheme` UNIQUE(`IdTimetable`,`IdCategory`,`IdParallel`,`IdDay`,`IdHour`),
   CONSTRAINT `PrimaryKey` PRIMARY KEY(`IdTimetable`,`IdTheme`,`IdCategory`,`IdParallel`,`IdDay`,`IdHour`),
   CONSTRAINT `DistributionTimetableDetail` FOREIGN KEY (`IdTheme`,`IdCategory`,`IdParallel`)
