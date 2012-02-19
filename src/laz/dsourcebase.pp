11f6db0a69b4d811ae8ffc95dca5a8851c577f10
edison <edison@68707deb-da3f-46f8-829b-9c434defd12b>
2012-02-19 17:10:22 +0000

    Update auto generated files
    git-svn-id: svn+ssh://mntedison/home/edison/LocalReps/ttg/trunk@754 68707deb-da3f-46f8-829b-9c434defd12b

--- src/laz/dsourcebase.pp
+++ src/laz/dsourcebase.pp
@@ -2,7 +2,7 @@
 unit dsourcebase;
 
 (*
-  19/02/2012 17:33
+  19/02/2012 18:09
 
   Warning:
 
--- src/laz/dsourcebaseconsts.pp
+++ src/laz/dsourcebaseconsts.pp
@@ -2,7 +2,7 @@
 unit dsourcebaseconsts;
 
 (*
-  14/02/2012 2:13
+  19/02/2012 18:09
 
   Warning:
 
@@ -23,88 +23,97 @@ uses
 
 resourcestring
 
-  STbActivity = 'Activities';
+  STbTheme = 'Themes';
   STbCategory = 'Categories';
-  STbCluster = 'Clusters';
+  STbParallel = 'Parallels';
   STbDay = 'Working Days';
   STbHour = 'Academic Hours';
+  STbCluster = 'Clusters';
   STbJoinedCluster = 'Joined Clusters';
-  STbParallel = 'Parallels';
-  STbRequirement = 'Requirements';
-  STbResource = 'Resources';
-  STbResourceRestriction = 'Resource Restrictions';
-  STbResourceRestrictionType = 'Types of Resource Restrictions';
   STbResourceType = 'Resource types';
-  STbTheme = 'Themes';
-  STbThemeRestriction = 'Theme Restrictions';
-  STbThemeRestrictionType = 'Types of Theme Restrictions';
+  STbResource = 'Resources';
+  STbResourceRestrictionType = 'Types of Resource Restrictions';
   STbTimeSlot = 'Time Slots';
+  STbActivity = 'Activities';
+  STbRequirement = 'Requirements';
+  STbThemeRestrictionType = 'Types of Theme Restrictions';
+  STbThemeRestriction = 'Theme Restrictions';
+  STbResourceRestriction = 'Resource Restrictions';
   STbTimetable = 'Timetables';
   STbTimetableDetail = 'Detail of Timetables';
+  STbTimetableResource = 'Resources of Timetables';
 
-  SFlActivity_Composition = 'Composition';
-  SFlActivity_IdCategory = 'Category';
-  SFlActivity_IdParallel = 'Parallel';
-  SFlActivity_IdTheme = 'Theme';
-  SFlCategory_AbCategory = 'Abbreviation';
+  SFlTheme_IdTheme = 'Id';
+  SFlTheme_NaTheme = 'Name';
   SFlCategory_IdCategory = 'Id';
   SFlCategory_NaCategory = 'Name';
-  SFlCluster_IdCategory = 'Category';
-  SFlCluster_IdParallel = 'Parallel';
+  SFlCategory_AbCategory = 'Abbreviation';
+  SFlParallel_IdParallel = 'Id';
+  SFlParallel_NaParallel = 'Name';
   SFlDay_IdDay = 'Id';
   SFlDay_NaDay = 'Name';
   SFlHour_IdHour = 'Id';
-  SFlHour_Interval = 'Interval';
   SFlHour_NaHour = 'Name';
-  SFlJoinedCluster_IdCategory = 'Category';
-  SFlJoinedCluster_IdCategory1 = 'Joined Category';
-  SFlJoinedCluster_IdParallel = 'Parallel';
-  SFlJoinedCluster_IdParallel1 = 'Joined Parallel';
+  SFlHour_Interval = 'Interval';
+  SFlCluster_IdCategory = 'Category';
+  SFlCluster_IdParallel = 'Parallel';
   SFlJoinedCluster_IdTheme = 'Theme';
-  SFlParallel_IdParallel = 'Id';
-  SFlParallel_NaParallel = 'Name';
-  SFlRequirement_IdCategory = 'Category';
-  SFlRequirement_IdParallel = 'Parallel';
-  SFlRequirement_IdResource = 'Assistant';
-  SFlRequirement_IdTheme = 'Theme';
-  SFlResourceRestrictionType_ColResourceRestrictionType = 'Color';
-  SFlResourceRestrictionType_IdResourceRestrictionType = 'Id';
-  SFlResourceRestrictionType_NaResourceRestrictionType = 'Name';
-  SFlResourceRestrictionType_ValResourceRestrictionType = 'Value';
-  SFlResourceRestriction_IdDay = 'Day';
-  SFlResourceRestriction_IdHour = 'Hour';
-  SFlResourceRestriction_IdResource = 'Resource';
-  SFlResourceRestriction_IdResourceRestrictionType = 'Restriction Type';
-  SFlResourceType_DefaultLimit = 'Default Limit';
+  SFlJoinedCluster_IdCategory = 'Category';
+  SFlJoinedCluster_IdParallel = 'Parallel';
+  SFlJoinedCluster_IdCategory1 = 'Joined Category';
+  SFlJoinedCluster_IdParallel1 = 'Joined Parallel';
   SFlResourceType_IdResourceType = 'IdResourceType';
   SFlResourceType_NaResourceType = 'Name';
-  SFlResource_AbResource = 'Abbreviation';
+  SFlResourceType_DefaultLimit = 'Default Limit';
   SFlResource_IdResource = 'Id';
+  SFlResource_IdResourceType = 'Type';
   SFlResource_NaResource = 'Name';
+  SFlResource_AbResource = 'Abbreviation';
   SFlResource_NumResource = 'Number';
-  SFlThemeRestrictionType_ColThemeRestrictionType = 'Color';
-  SFlThemeRestrictionType_IdThemeRestrictionType = 'Id';
-  SFlThemeRestrictionType_NaThemeRestrictionType = 'Name';
-  SFlThemeRestrictionType_ValThemeRestrictionType = 'Value';
-  SFlThemeRestriction_IdDay = 'Day';
-  SFlThemeRestriction_IdHour = 'Hour';
-  SFlThemeRestriction_IdTheme = 'Theme';
-  SFlThemeRestriction_IdThemeRestrictionType = 'Restriction Type';
-  SFlTheme_IdTheme = 'Id';
-  SFlTheme_NaTheme = 'Name';
+  SFlResourceRestrictionType_IdResourceRestrictionType = 'Id';
+  SFlResourceRestrictionType_IdResourceType = 'Resource Type';
+  SFlResourceRestrictionType_NaResourceRestrictionType = 'Name';
+  SFlResourceRestrictionType_ColResourceRestrictionType = 'Color';
+  SFlResourceRestrictionType_ValResourceRestrictionType = 'Value';
   SFlTimeSlot_IdDay = 'Day';
   SFlTimeSlot_IdHour = 'Hour';
+  SFlActivity_IdTheme = 'Theme';
+  SFlActivity_IdCategory = 'Category';
+  SFlActivity_IdParallel = 'Parallel';
+  SFlActivity_Composition = 'Composition';
+  SFlRequirement_IdTheme = 'Theme';
+  SFlRequirement_IdCategory = 'Category';
+  SFlRequirement_IdParallel = 'Parallel';
+  SFlRequirement_IdResource = 'Resource';
+  SFlRequirement_NumRequirement = 'Number';
+  SFlThemeRestrictionType_IdThemeRestrictionType = 'Id';
+  SFlThemeRestrictionType_NaThemeRestrictionType = 'Name';
+  SFlThemeRestrictionType_ColThemeRestrictionType = 'Color';
+  SFlThemeRestrictionType_ValThemeRestrictionType = 'Value';
+  SFlThemeRestriction_IdTheme = 'Theme';
+  SFlThemeRestriction_IdDay = 'Day';
+  SFlThemeRestriction_IdHour = 'Hour';
+  SFlThemeRestriction_IdThemeRestrictionType = 'Restriction Type';
+  SFlResourceRestriction_IdResource = 'Resource';
+  SFlResourceRestriction_IdDay = 'Day';
+  SFlResourceRestriction_IdHour = 'Hour';
+  SFlResourceRestriction_IdResourceRestrictionType = 'Restriction Type';
+  SFlTimetable_IdTimetable = 'Id';
+  SFlTimetable_TimeIni = 'Initial Time';
+  SFlTimetable_TimeEnd = 'End Time';
+  SFlTimetable_Summary = 'Summary';
+  SFlTimetableDetail_IdTimetable = 'Timetable';
+  SFlTimetableDetail_IdTheme = 'Theme';
   SFlTimetableDetail_IdCategory = 'Category';
+  SFlTimetableDetail_IdParallel = 'Parallel';
   SFlTimetableDetail_IdDay = 'Day';
   SFlTimetableDetail_IdHour = 'Hour';
-  SFlTimetableDetail_IdParallel = 'Parallel';
-  SFlTimetableDetail_IdTheme = 'Theme';
-  SFlTimetableDetail_IdTimetable = 'Timetable';
   SFlTimetableDetail_Session = 'Session';
-  SFlTimetable_IdTimetable = 'Id';
-  SFlTimetable_Summary = 'Summary';
-  SFlTimetable_TimeEnd = 'End Time';
-  SFlTimetable_TimeIni = 'Initial Time';
+  SFlTimetableResource_IdTimetable = 'Timetable';
+  SFlTimetableResource_IdTheme = 'Theme';
+  SFlTimetableResource_IdCategory = 'Category';
+  SFlTimetableResource_IdParallel = 'Parallel';
+  SFlTimetableResource_IdResource = 'Resource';
 
 implementation
 
