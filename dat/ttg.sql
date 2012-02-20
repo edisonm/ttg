d1988347a18e98489afcc2e8b45917d400b62bbd
edison <edison@68707deb-da3f-46f8-829b-9c434defd12b>
2012-02-20 16:53:50 +0000

    Updating database
    git-svn-id: svn+ssh://mntedison/home/edison/LocalReps/ttg/trunk@769 68707deb-da3f-46f8-829b-9c434defd12b

--- dat/ttg.sql
+++ dat/ttg.sql
@@ -1,6 +1,6 @@
 /* -*- mode: SQL; -*-
 
-  19/02/2012 20:35
+  20/02/2012 16:11
 
   Warning:
 
@@ -51,6 +51,15 @@ CREATE TABLE IF NOT EXISTS `Activity`(
   CONSTRAINT `ThemeActivity` FOREIGN KEY (`IdTheme`)
     REFERENCES `Theme`(`IdTheme`) ON UPDATE RESTRICT ON DELETE RESTRICT
 ); /* Activities */
+CREATE TABLE IF NOT EXISTS `Period`(
+    `IdDay` INTEGER NOT NULL /* Day Id */,
+    `IdHour` INTEGER NOT NULL /* Hour Id */,
+  CONSTRAINT `PrimaryKey` PRIMARY KEY(`IdDay`,`IdHour`),
+  CONSTRAINT `DayPeriod` FOREIGN KEY (`IdDay`)
+    REFERENCES `Day`(`IdDay`) ON UPDATE RESTRICT ON DELETE RESTRICT,
+  CONSTRAINT `HourPeriod` FOREIGN KEY (`IdHour`)
+    REFERENCES `Hour`(`IdHour`) ON UPDATE RESTRICT ON DELETE RESTRICT
+); /* Periods */
 CREATE TABLE IF NOT EXISTS `ResourceType`(
     `IdResourceType` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT /* Resource Type Id */,
     `NaResourceType` VARCHAR(15) NOT NULL UNIQUE /* Name */,
@@ -72,15 +81,19 @@ CREATE TABLE IF NOT EXISTS `ResourceRestrictionType`(
     `ColResourceRestrictionType` INTEGER NOT NULL /* Restriction Type Color */,
     `ValResourceRestrictionType` INTEGER NOT NULL /* Restriction Type Value */
 ); /* Types of Resource Restrictions */
-CREATE TABLE IF NOT EXISTS `Period`(
+CREATE TABLE IF NOT EXISTS `ResourceRestriction`(
+    `IdResource` INTEGER NOT NULL /* Resource Id */,
     `IdDay` INTEGER NOT NULL /* Day Id */,
     `IdHour` INTEGER NOT NULL /* Hour Id */,
-  CONSTRAINT `PrimaryKey` PRIMARY KEY(`IdDay`,`IdHour`),
-  CONSTRAINT `DayPeriod` FOREIGN KEY (`IdDay`)
-    REFERENCES `Day`(`IdDay`) ON UPDATE RESTRICT ON DELETE RESTRICT,
-  CONSTRAINT `HourPeriod` FOREIGN KEY (`IdHour`)
-    REFERENCES `Hour`(`IdHour`) ON UPDATE RESTRICT ON DELETE RESTRICT
-); /* Time Slots */
+    `IdResourceRestrictionType` INTEGER NOT NULL /* Resource Restriction Type Id */,
+  CONSTRAINT `PrimaryKey` PRIMARY KEY(`IdResource`,`IdDay`,`IdHour`),
+  CONSTRAINT `PeriodResourceRestriction` FOREIGN KEY (`IdDay`,`IdHour`)
+    REFERENCES `Period`(`IdDay`,`IdHour`) ON UPDATE RESTRICT ON DELETE RESTRICT,
+  CONSTRAINT `ResourceResourceRestriction` FOREIGN KEY (`IdResource`)
+    REFERENCES `Resource`(`IdResource`) ON UPDATE CASCADE ON DELETE RESTRICT,
+  CONSTRAINT `ResourceRestrictionTypeResourceRestriction` FOREIGN KEY (`IdResourceRestrictionType`)
+    REFERENCES `ResourceRestrictionType`(`IdResourceRestrictionType`) ON UPDATE RESTRICT ON DELETE RESTRICT
+); /* Resource Restrictions */
 CREATE TABLE IF NOT EXISTS `Requirement`(
     `IdTheme` INTEGER NOT NULL /* Theme Id */,
     `IdCategory` INTEGER NOT NULL /* Category Id */,
@@ -117,26 +130,13 @@ CREATE TABLE IF NOT EXISTS `ThemeRestriction`(
     `IdHour` INTEGER NOT NULL /* Hour Id */,
     `IdThemeRestrictionType` INTEGER NOT NULL /* Theme Restriction Type Id */,
   CONSTRAINT `PrimaryKey` PRIMARY KEY(`IdTheme`,`IdDay`,`IdHour`),
+  CONSTRAINT `PeriodThemeRestriction` FOREIGN KEY (`IdDay`,`IdHour`)
+    REFERENCES `Period`(`IdDay`,`IdHour`) ON UPDATE RESTRICT ON DELETE RESTRICT,
   CONSTRAINT `ThemeRestrictionTypeThemeRestriction` FOREIGN KEY (`IdThemeRestrictionType`)
     REFERENCES `ThemeRestrictionType`(`IdThemeRestrictionType`) ON UPDATE RESTRICT ON DELETE RESTRICT,
   CONSTRAINT `ThemeThemeRestriction` FOREIGN KEY (`IdTheme`)
-    REFERENCES `Theme`(`IdTheme`) ON UPDATE RESTRICT ON DELETE RESTRICT,
-  CONSTRAINT `PeriodThemeRestriction` FOREIGN KEY (`IdDay`,`IdHour`)
-    REFERENCES `Period`(`IdDay`,`IdHour`) ON UPDATE RESTRICT ON DELETE RESTRICT
+    REFERENCES `Theme`(`IdTheme`) ON UPDATE RESTRICT ON DELETE RESTRICT
 ); /* Theme Restrictions */
-CREATE TABLE IF NOT EXISTS `ResourceRestriction`(
-    `IdResource` INTEGER NOT NULL /* Resource Id */,
-    `IdDay` INTEGER NOT NULL /* Day Id */,
-    `IdHour` INTEGER NOT NULL /* Hour Id */,
-    `IdResourceRestrictionType` INTEGER NOT NULL /* Resource Restriction Type Id */,
-  CONSTRAINT `PrimaryKey` PRIMARY KEY(`IdResource`,`IdDay`,`IdHour`),
-  CONSTRAINT `ResourceResourceRestriction` FOREIGN KEY (`IdResource`)
-    REFERENCES `Resource`(`IdResource`) ON UPDATE CASCADE ON DELETE RESTRICT,
-  CONSTRAINT `ResourceRestrictionTypeResourceRestriction` FOREIGN KEY (`IdResourceRestrictionType`)
-    REFERENCES `ResourceRestrictionType`(`IdResourceRestrictionType`) ON UPDATE RESTRICT ON DELETE RESTRICT,
-  CONSTRAINT `PeriodResourceRestriction` FOREIGN KEY (`IdDay`,`IdHour`)
-    REFERENCES `Period`(`IdDay`,`IdHour`) ON UPDATE RESTRICT ON DELETE RESTRICT
-); /* Resource Restrictions */
 CREATE TABLE IF NOT EXISTS `Timetable`(
     `IdTimetable` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT /* Timetable Id */,
     `TimeIni` DATETIME NOT NULL /* Initial Time */,
